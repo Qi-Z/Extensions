400ea39fbcb7bee7aacb3887587cff7792952175
sophia yan <sophiaxueting@gmail.com>
2015-05-09 21:33:26 -0500

    add send messages

--- background.js
+++ background.js
@@ -30,5 +30,13 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 chrome.pageAction.onClicked.addListener(function(){
-  alert("Hi!")
+  console.log("Before sending message")
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {message: "message sent from background"}, function(response) {
+      console.log(response.feedback);
+    });
+  });
+
+  console.log("After sending message")
+  //alert("After sending message")
 });
--- content.js
+++ content.js
@@ -1,10 +1,24 @@
 /* Listen for messages */
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    /* If the received message has the expected format... */
-    if (msg.text && (msg.text == "report_back")) {
-        /* Call the specified callback, passing 
-           the web-pages DOM content as argument */
-        sendResponse(document.all[0].outerHTML);
-        alert(document.all[0].outerHTML)
+// chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+//     /* If the received message has the expected format... */
+//     if (msg.text && (msg.text == "report_back")) {
+//         /* Call the specified callback, passing 
+//            the web-pages DOM content as argument */
+//         sendResponse(document.all[0].outerHTML);
+//         alert(document.all[0].outerHTML)
+//     }
+// });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    // console.log(sender.tab ?
+    //             "from a content script:" + sender.tab.url :
+    //             "from the extension");
+    if (request.message == "message sent from background")
+    {
+    	var comments = document.getElementsByClassName("review-content");
+    	alert(comments[0].textContent);
+    	sendResponse({feedback: "response from content"});
     }
-});
+  	//console.log("one iteration")
+  });
